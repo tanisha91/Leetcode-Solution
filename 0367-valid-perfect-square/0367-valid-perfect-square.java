89d4aba63c912332acffa48aa2d025ee8457620a
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2023-01-17 07:59:51 -0800

    Time: 0 ms (100.00%), Space: 39.4 MB (50.64%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start=1;
+        long end=num;
+        while(start<=end){
+          long mid=(start+end)/2;
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid<num)
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return false;
+    }
+}
