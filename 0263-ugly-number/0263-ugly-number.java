4c7c23dc3e2475fbb521f6d245f7a46ca1e70a73
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2023-01-27 22:06:30 -0800

    Time: 1 ms (100.00%), Space: 39.2 MB (94.21%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isUgly(int n) {
        if(n<=0) return false;
         if(n==1) return true;
-        if(n%2==0) return isUgly(n/2);
-         if(n%3==0) return isUgly(n/3);
-         if(n%5==0) return isUgly(n/5);
+        while(n%2==0) return isUgly(n/2);
+         while (n%3==0) return isUgly(n/3);
+          while(n%5==0) return isUgly(n/5);
            return false;
             }
         }
