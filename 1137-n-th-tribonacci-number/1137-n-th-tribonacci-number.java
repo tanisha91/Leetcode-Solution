cae9edb20f661ff22d87572b655fa1f7d938524e
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2023-01-30 01:50:35 -0800

    Time: 0 ms (100.00%), Space: 38.8 MB (90.39%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,15 +1,14 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n<3)
-            return n==0? 0:1;
-        int res[]=new int[n+1];
-         res[0]=0;
-       res[1]=1;
-         res[2]=1;
+       if(n<3)
+           return n==0?0:1;
+        int tri[]=new int[n+1];
+        tri[0]=0;
+        tri[1]=1;
+        tri[2]=1;
         for(int i=3;i<=n;i++)
-            res[i]=res[i-1]+res[i-2]+res[i-3];
-        
-           return res[n];
-        
+            tri[i]=tri[i-1]+tri[i-2]+tri[i-3];
+            
+        return tri[n];
     }
 }
