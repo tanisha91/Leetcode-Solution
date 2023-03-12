59c5c2122b9d764fc35175058a42e67aec8af4e8
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2023-03-11 23:48:10 -0800

    Time: 32 ms (100.00%), Space: 51.1 MB (100.00%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxScore(int[] nums) {
         Arrays.sort(nums);
-        //ArrayList<Integer>a=new ArrayList<>();
+       // ArrayList<Integer>a=new ArrayList<>();
        long sum=0;
         long max=0;
        for(int i=nums.length-1;i>=0;--i){
@@ -13,7 +13,7 @@ class Solution {
             if(sum>0){
                 max++;
             }
-           else break;
+          // else break;
        }
         return (int)max;
     }
