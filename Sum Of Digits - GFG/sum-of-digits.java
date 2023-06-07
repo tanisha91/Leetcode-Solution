5fc7db22e33068a48fc860a90a8be82b9653ed57
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2023-06-07 20:27:58 +0530

    Added solution - LeetHub

---
+++ Sum Of Digits - GFG/sum-of-digits.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfDigits(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int sumOfDigits(int N) {
+        // code here
+        int sum=0;
+        while(N>0){
+        int rem=N%10;
+        sum+=rem;
+        N/=10;
+        }
+        return sum;
+    }
+}
+       
