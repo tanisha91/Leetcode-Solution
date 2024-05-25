f72a61f82d9fb5c69b88255a27650bc147a070f5
Tanisha Mangla <119071904+tanisha91@users.noreply.github.com>
2024-05-25 10:00:49 +0530

    Time: 4 ms (24.39%), Space: 46.9 MB (80.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,39 @@
 class Solution {
+    ArrayList<ArrayList<Integer>> al=new ArrayList<>();
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        int cnt=0;
         boolean vis[]=new boolean[n];
-        for(int i=0;i<isConnected.length;i++){
+        list(al,n,isConnected);
+        int cnt=0;
+        for(int i=0;i<n;i++){
             if(!vis[i]){
                 cnt++;
-                dfs(i,vis,isConnected);
+                dfs(i,vis,al);
             }
         }
-        return cnt;
+   return cnt;
+    
     }
     
-    void dfs(int node,boolean vis[],int[][]Con){
-        if(vis[node]){
-            return ;
+    void list(ArrayList<ArrayList<Integer>> al,int n,int[][] isConnected){  
+        for(int i=0;i<n;i++){
+            al.add(new ArrayList<>());
         }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                al.get(i).add(j);
+                al.get(j).add(i);
+            }
+            }
+            }
+    }
+    void dfs(int node,boolean vis[],ArrayList<ArrayList<Integer>> al){
         vis[node]=true;
-        for(int i=0;i<Con[0].length;i++){
-            if(Con[node][i]==1){
-                dfs(i,vis,Con);
+        for(Integer it : al.get(node)){
+            if(vis[it]==false){
+                vis[it]=true;
+                dfs(it,vis,al);
             }
         }
     }
